b2c3710fa3fa386751ceb51afff4572a2884ae78
JCortezAyala <40844882+jcorteza@users.noreply.github.com>
2018-10-28 21:44:45 -0500

    update

--- index.js
+++ index.js
@@ -1,39 +1,81 @@
+const colorWords = require('./colors.js');
 const Word = require('./word.js')
 const prompt = require('prompt');
 const wordSet = process.argv[2];
-const colorWords = require('./colors.js');
-let wordsInPlay = [];
 
-function setupGame(){
+function wordGuess() {
+    let wordsInPlay = [];
+    let wrongsLeft = 5;
+    let losses = 3;
+    let won;
+    let guessObject = {
+        name: 'guess',
+        description: 'Enter your letter guess',
+        type: 'string',
+        pattern: /^[a-zA-z]$/,
+        message: 'Your guess must only include a single letter.',
+        required: true
+    };
+    if(wordSet === undefined || wordSet === 0) throw 'You must enter one of the word categories to play: classic, fruit, earth, precious, animals, food, trees, or flowers';
+    setupGame(wordsInPlay);
+    prompt.start();
+    wordsInPlay.forEach((word) =>{
+        let solved = false;
+        if(losses > 0){
+            console.log(`This round's word is ${word}.`);
+            const wordThisRound = new Word(word);
+            wordThisRound.setupWord();
+            do {
+                wordThisRound.updateWordDisplay();
+                new Promise(function(resolve) {
+                    prompt.get(guessObject, (err, result) => {
+                        if(err) throw err;
+                        let userGuess;
+                        const letterGuessed = result.guess.toLowerCase();
+                        if (wordThisRound.wordObject.hasOwnProperty(letterGuessed) && wordThisRound.wordObject[letterGuessed].guessed === false) {
+                            userGuess = true;
+                            wordThisRound.wordObject[letterGuessed].guessed = true;
+                            wordThisRound.wordObject[letterGuessed].changeDisplay();
+                        }
+                        else {
+                            userGuess = false;
+                            wrongsLeft--;
+                        }
+                        resolve({gussedRight: userGuess, letter: letterGuessed});
+                    });
+                }).then(function(promise){
+                    gameFeedback(promise.gussedRight, promise.letter);
+                });
+                --wrongsLeft;
+                for(let key in wordThisRound.wordObject){
+                    if(wordThisRound.wordObject[key].guessed === true) solved = true;
+                    else {
+                        solved = false; 
+                        return;
+                    }
+                }
+            } while(wrongsLeft > 0 || solved !== true);
+            if(wrongsLeft === 0) losses--;
+        }
+    });
+    console.log('Reached the end of the words.');
+    let message;
+    (won)? message = '====================/YOU WON=========================' : message = '====================/YOU LOST========================'
+    console.log('\n=====================================================');
+    console.log('====================/GAME OVER/======================');
+    console.log('=====================================================');
+    console.log(message);
+    console.log('=====================================================');
+}
+function setupGame(array){
     const thisGameArray = colorWords[wordSet];
-    while(wordsInPlay.length < thisGameArray.length){
+    while(array.length < thisGameArray.length){
         const index = Math.floor(Math.random() * thisGameArray.length);
-        if(!wordsInPlay.includes(thisGameArray[index])) wordsInPlay.push(thisGameArray[index]);
+        if(!array.includes(thisGameArray[index])) array.push(thisGameArray[index]);
     }
 }
-function getUserGuess(word, guessObject, wrongs){
-    word.updateWordDisplay();
-    prompt.start();
-    return new Promise(function(resolve) {
-        prompt.get(guessObject, (err, result) => {
-            if(err) throw err;
-            const letterGuessed = result.guess.toLowerCase();
-            let userGuess;
-            if (word.wordObject.hasOwnProperty(letterGuessed) && word.wordObject[letterGuessed].guessed === false) {
-                userGuess = true;
-                word.wordObject[letterGuessed].guessed = true;
-                word.wordObject[letterGuessed].changeDisplay();
-            }
-            else {
-                userGuess = false;
-                wrongs--;
-            }
-            resolve({gussedRight: userGuess, letter: letterGuessed, wrongs: wrongs});
-        });
-    });
-}
-function gameFeedback(guessed, letter, wrongs){
-    console.log('=================/YOUR LAST GUESS/===================');
+function gameFeedback(guessed, letter){
+    console.log('\n=================/YOUR LAST GUESS/===================');
     if(guessed){
         console.log(`${letter}: correct\n`)
         console.log('Great guess!\n')
@@ -42,57 +84,6 @@ function gameFeedback(guessed, letter, wrongs){
         console.log(`${letter}: wrong`)
         console.log('Better luck next time!\n')
     }
-    console.log(`Wrong guesses left: ${wrongs}\n`);
-}
-function wordGuess() {
-    let wrongsLeft = 5;
-    let losses = 4;
-    if(wordSet === undefined || wordSet === 0) throw 'You must enter one of the word categories to play: classic, fruit, earth, precious, animals, food, trees, or flowers';
-    setupGame();
-    if(losses > 0){
-        let wordThisRound;
-        let guessObject = {
-            name: 'guess',
-            description: 'Enter your letter guess',
-            type: 'string',
-            pattern: /^[a-zA-z]$/,
-            message: 'Your guess must only include a single letter.',
-            required: true
-        };
-        for(i = 0; i < wordsInPlay.length; i++){
-            console.log(`This round's word is ${wordsInPlay[i]}.`);
-            wordThisRound = new Word(wordsInPlay[i]);
-            wordThisRound.setupWord();
-            do {
-                getUserGuess(wordThisRound, guessObject, wrongsLeft).then(function(promise){
-                    wrongsLeft = promise.wrongs;
-                    gameFeedback(promise.gussedRight, promise.letter, wrongsLeft);
-                });
-            } while (wrongsLeft > 0);
-        }
-        /*wordsInPlay.forEach((colorWord) =>{
-            wordThisRound = new Word(colorWord);
-            wordThisRound.setupWord();
-            prompt.start();
-            prompt.get(guess, (err, result) => {
-                if(err) throw err;
-                console.log(result);
-            //     updateDisplay(result, wordThisRound);
-            });
-        });*/
-    }
-    else {
-        console.log('Game Over');
-    }
-
+    console.log(`Wrong guesses left: ${wrongsLeft}\n`);
 }
 wordGuess();
-/*
-Word {
-  wordAnswer: [ 'b', 'l', 'u', 'e' ],  wordDisplay: [ '_', '_', '_', '_' ],
-  wordObject:   { b: Letter { guessed: false, display: '_', right: 'b' },
-     l: Letter { guessed: false, display: '_', right: 'l' },
-     u: Letter { guessed: false, display: '_', right: 'u' },
-     e: Letter { guessed: false, display: '_', right: 'e' } },
-  setupWord: [Function] }
-  */
--- word.js
+++ word.js
@@ -1,23 +1,33 @@
 const Letter = require('./letter.js');
 let Word = function(word){
     this.wordAnswer = word.split('');
-    this.wordDisplay = [];
+    // this.wordDisplay = [];
     this.wordObject = {};
     this.setupWord = function() {
-        this.wordAnswer.forEach((char) => {
+        this.wordDisplay = this.wordAnswer.map((char) => {
             const thisLetter = new Letter(char);
-            this.wordDisplay.push(thisLetter.display);
             this.wordObject[char] = thisLetter;
+            return thisLetter.display;
+            // this.wordDisplay.push(thisLetter.display);
         });
+        // this.wordAnswer.forEach((char) => {
+        //     const thisLetter = new Letter(char);
+        //     this.wordDisplay.push(thisLetter.display);
+        //     this.wordObject[char] = thisLetter;
+        // });
     }
     this.updateWordDisplay = function() {
-        this.wordDisplay = [];
-        this.wordAnswer.forEach((char) => {
+        this.wordDisplay = this.wordAnswer.map((char) => {
             console.log(this.wordObject[char].display);
-            this.wordDisplay.push(this.wordObject[char].display);
+            return this.wordObject[char].display;
+            // this.wordDisplay.push(this.wordObject[char].display);
         });
+        // this.wordAnswer.forEach((char) => {
+        //     console.log(this.wordObject[char].display);
+        //     this.wordDisplay.push(this.wordObject[char].display);
+        // });
         console.log(`new wordDisplay array is ${this.wordDisplay}`)
-        console.log('=================/WORD GUESS-COLORS/===================');
+        console.log('\n=================/WORD GUESS-COLORS/===================');
         console.log('Take your guess!!!\n');
         console.log(`${this.wordDisplay.join(' ')}\n`);
     }
